221fe2ad177523f2697d40457c4721624637f186
Brandon Baskett <24949309+BBaskett@users.noreply.github.com>
2021-04-12 14:59:35 -0400

    v1

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,105 @@
 <script lang="ts">
-	export let name: string;
+  export let currencies: [string], name: string, storage_name: string;
+  import { BASE, MASTER } from "./stores";
+  import { onMount } from "svelte";
+
+  let error;
+  const storage = window.localStorage.getItem(storage_name);
+
+  // Update storage when currency changes
+  $: window.localStorage.setItem(storage_name, $BASE);
+
+  async function getExchangeRates(base: string) {
+    const key: string = "cd08a31a4b50cbf89c21208b";
+    try {
+      const response = await fetch(
+        `https://v6.exchangerate-api.com/v6/${key}/latest/${base}`
+      );
+      return await response.json();
+    } catch (e) {
+      console.error(e);
+      return (error = e);
+    }
+  }
+
+  onMount(async () => {
+    if (storage) {
+      // Initialize currency default from storage
+      BASE.set(storage);
+    }
+  });
 </script>
 
+<header>
+  <h1>{name}</h1>
+</header>
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <div id="master-controls">
+    <input type="number" bind:value={$MASTER} />
+    <select bind:value={$BASE}>
+      <option value="" selected disabled>Currency</option>
+      {#each currencies as currency}
+        <option value={currency}>{currency}</option>
+      {/each}
+    </select>
+  </div>
+  {#if error}
+    <p>Error: {error}</p>
+  {/if}
+  {#await getExchangeRates($BASE)}
+    <p id="loading">loading...</p>
+  {:then data}
+    <div id="conversions">
+      {#each currencies as currency}
+        <form>
+          <input
+            type="number"
+            name={currency}
+            class={data.conversion_rates[currency] > 1 ? "good" : "bad"}
+            value={data.conversion_rates[currency] * $MASTER}
+            readonly
+          />
+          <label for={currency}>{currency}</label>
+        </form>
+      {/each}
+    </div>
+  {/await}
 </main>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+<style type="text/scss">
+  header {
+    text-align: center;
+    padding: 1rem 0;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  main {
+    #master-controls {
+      display: flex;
+      justify-content: center;
+      padding: 1rem 0;
+      * + * {
+        margin-left: 1rem;
+      }
+    }
+    #conversions {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-around;
+      form {
+        display: flex;
+        flex-direction: column;
+        justify-content: stretch;
+        padding: 0.5rem;
+        input {
+          border: none;
+        }
+        label {
+          border-top: 1px solid var(--primary);
+        }
+      }
+    }
+    #loading {
+      text-align: center;
+    }
+  }
 </style>
