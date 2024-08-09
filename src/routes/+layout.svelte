37195c6ad1ee27170045ea462c1f16e437fb0838
Brandon Baskett <24949309+BBaskett@users.noreply.github.com>
2024-08-09 16:34:44 -0400

    update v1.0

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,6 @@
 <script>
+  export const prerender = true;
+  export const ssr = false;
   import "../app.css";
 </script>
 
--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
-import adapter from "@sveltejs/adapter-auto";
-import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -17,7 +16,7 @@ const config = {
       // these options are set automatically — see below
       pages: "build",
       assets: "build",
-      fallback: undefined,
+      fallback: "200.html",
       precompress: false,
       strict: true,
     }),
