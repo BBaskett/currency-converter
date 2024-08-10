512c688bb8a2223d36fb79d306b62e0113e71285
Brandon Baskett <24949309+BBaskett@users.noreply.github.com>
2024-08-10 07:35:27 -0400

    Delete .svelte-kit directory

--- .svelte-kit/output/server/entries/pages/_layout.svelte.js
+++
@@ -1,7 +0,0 @@
-import { c as create_ssr_component } from "../../chunks/ssr.js";
-const Layout = create_ssr_component(($$result, $$props, $$bindings, slots) => {
-  return `${$$result.head += `<!-- HEAD_svelte-lq2ew8_START -->${$$result.title = `<title>Currency Converter</title>`, ""}<!-- HEAD_svelte-lq2ew8_END -->`, ""} ${slots.default ? slots.default({}) : ``}`;
-});
-export {
-  Layout as default
-};
--- .svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.BcWVHtTB.js","_app/immutable/chunks/scheduler.DD6rCPCs.js","_app/immutable/chunks/index.CB7uf5yc.js","_app/immutable/chunks/entry.Dn0yn2M2.js"];
-export const stylesheets = [];
-export const fonts = [];
