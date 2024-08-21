578188939e6144970044f2a9537eae20f42569f8
Brandon Baskett <24949309+BBaskett@users.noreply.github.com>
2024-08-21 15:01:29 -0400

    svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -16,10 +16,13 @@ const config = {
       // these options are set automatically — see below
       pages: "build/",
       assets: "build",
-      fallback: "404.html",
+      fallback: "index.html",
       precompress: false,
       strict: true,
     }),
+  },
+  paths: {
+    base: process.argv.includes("dev") ? "" : process.env.BASE_PATH,
   },
 };
 
